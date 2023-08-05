ca582a1fe008b067924c30728c884d7ba8c8837b
jkaringada <jkaringada@gmail.com>
2023-08-05 16:30:41 +1000

    creating  my first new branch

---
+++ src/main/java/com/git/testgit/config/ObjectMapperConfig.java
@@ -0,0 +1,19 @@
+package com.git.testgit.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Primary
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        JavaTimeModule module = new JavaTimeModule();
+        return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL).registerModule(module);
+    }
+}
